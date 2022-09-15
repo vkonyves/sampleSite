9fab422243d063bbeb17abf0888de01447c7c2d9
Vera Konyves <vera.konyves@gmail.com>
2022-09-15 23:43:34 +0200

    Fixed button bug

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ var button = document.getElementById('theButton');
 
 //add event listener
 button.addEventListener('click', function(event) {
-  alort("You are awesome!");
+  alert("You are awesome!");
 });
