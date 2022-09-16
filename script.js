7b40fe47ff521a3607c46c8a3e66fb0479b56ca1
Vera Konyves <vera.konyves@gmail.com>
2022-09-16 14:34:20 +0200

    Modified site look

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ var button = document.getElementById('theButton');
 
 //add event listener
 button.addEventListener('click', function(event) {
-  alert("You are awesome!");
+  alert("Actions have consequences... first rule of life.");
 });
